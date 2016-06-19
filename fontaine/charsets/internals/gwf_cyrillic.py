57c8d73f8500c432e2856cb60285effeaf68f400
Alexei Vanyashin <alexeiva@users.noreply.github.com>
2016-06-19 16:55:29 -0400

    Update to GWF Cyrillic encoding v.01

--- fontaine/charsets/internals/gwf_cyrillic.py
+++ fontaine/charsets/internals/gwf_cyrillic.py
@@ -6,7 +6,8 @@ class Charset:
 
     def glyphs(self):
         # cyrillic subset from http://code.google.com/p/googlefontdirectory/source/browse/tools/subset/subset.py
-        glyphs = range(0x400, 0x460) + [0x490, 0x491, 0x4b0, 0x4b1]
+        glyphs = range(0x400, 0x460) + [0x490, 0x491, 0x20B4, 0x20BD, 0x2116]
+        
         return glyphs
 
 
