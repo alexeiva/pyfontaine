93dd2b53708d3adcb04d6295f5b885285ea33a46
Alexei Vanyashin <alexeiva@users.noreply.github.com>
2016-06-19 16:52:34 -0400

    Proposed changes to Basic Cyrillic encoding v.01
    Characters removed: 
    — Ұ, 04B0, CYRILLIC SMALL LETTER STRAIGHT U WITH STROKE
    — ұ, 04B1, CYRILLIC CAPITAL LETTER STRAIGHT U WITH STROKE
    
    Comments: These characters provide partial support for Kazakh. 
    The required unique characters to be added for full Kazakh support are the following:
    
    ә,Ә,ғ,Ғ,қ,Қ,ң,Ң,ө,Ө,ұ,Ұ,ү,Ү,һ,Һ
    Corresponding Unicode values: 04D9,04D8,0493,0492,049B,049A,04A3,04A2,04E9,04E8,04B1,04B0,04AF,04AE,04BB,04BA
    
    Kazakh language (11M) support should be in the Cyrillic Plus charset, alongside Uzbek, Kyrgyz, etc.
    
    Characters added:
    + ₴, 20B4, HRYVNIA SIGN
    + ₽, 20BD, RUBLE SIGN

--- fontaine/charsets/internals/basiccyrillic.py
+++ fontaine/charsets/internals/basiccyrillic.py
@@ -6,8 +6,8 @@ class Charset:
     glyphs  = range(0x0400, 0x045f)
     glyphs += [0x0490,
                0x0491,
-               0x04B0,
-               0x04B1,
+               0x20B4,
+               0x20BD,
                0x2116,
               ]
 
